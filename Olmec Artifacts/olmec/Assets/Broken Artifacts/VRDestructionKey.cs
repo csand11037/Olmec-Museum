cb38fe370394ef06c12b843694579ffd9ddae00c
csand11037 <78234788+csand11037@users.noreply.github.com>
2024-04-24 21:10:10 -0500

    added breaking script

---
+++ Olmec Artifacts/olmec/Assets/Broken Artifacts/VRDestructionKey.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRDestructionKey : MonoBehaviour
+{
+
+    public GameObject breakVersion;
+    public float bForce = 1f;
+    protected Rigidbody rb;
+    private int active = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (rb.velocity.magnitude > bForce && active == 0)
+        {
+            active++;
+            Instantiate(breakVersion, transform.position, transform.rotation);
+            rb.AddExplosionForce(10f, Vector3.zero, 0f);
+            Destroy(gameObject);
+            
+
+
+
+        }
+
+
+
+    }
+}
